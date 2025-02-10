e6d3215a61ef9b00deb0aeacc968539851e70067
filiphnatek <filip.hnatek@student.ossp.cz>
2025-02-10 10:02:19 +0100

    vzhled (template + static slozka)

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello world"
+    return render_template('index.html')
+
+@app.route("/<jmeno>")
+def hello(jmeno):
+    return f"<h1>Hello, {jmeno}!</h1>"
+
+
+def nasobeni(a, b):
+    return a * b
+
+@app.route("/nasobeni/<a>/<b>")
+def nasobeni_route(a, b):
+    try:
+        return f"{int(a) * int(b)} toto je vysledek"
+    except ValueError:
+        return "nejsou cisla"
 
 if __name__== "__main__":
-    app.run()
+    app.run(debug=True)
